1633544bfae38f4678befddf17dc47f111a979d6
G4bzh <jedemissionedusport@hotmail.fr>
2016-05-30 14:58:17 +0200

    font print

--- c8.h
+++ c8.h
@@ -9,32 +9,26 @@
 
 #include <stdint.h>
 
-#define MEMORY_SIZE      4096
-#define ROM_SIZE         512
-#define STACK_SIZE       16
-#define SCREEN_SIZE      2048
+#define C8_STACK_SIZE       16
+#define C8_SCREEN_SIZE      2048
 
-#define SCREEN_WIDTH     64
-#define SCREEN_HEIGHT    32
-
-#define START_OFFSET     0x200
-#define START_OFFSET_ETI 0x600
- 
-#define N_REGISTERS      16
-
+#define C8_REGISTERS        16
+#define C8_KEYS             16
 
 
 
 typedef struct _C8
 {
-  uint8_t V[N_REGISTERS];      /* General Purpose Registers (Vx) */
-  uint16_t I;                  /* Memory Register */
-  uint8_t delay;               /* Delay Timer Register */
-  uint8_t sound;               /* Sound Timer Register */
-  uint16_t PC;                 /* Progranm Counter */
-  uint16_t stack[STACK_SIZE];  /* Stack */
-  uint8_t SP;                  /* Stack Pointer */
-  uint8_t screen[SCREEN_SIZE]; /* Display */
+  uint8_t V[C8_REGISTERS];          /* General Purpose Registers (Vx) */
+  uint16_t I;                       /* Memory Register */
+  uint8_t delay;                    /* Delay Timer Register */
+  uint8_t sound;                    /* Sound Timer Register */
+  uint16_t PC;                      /* Progranm Counter */
+  uint16_t stack[C8_STACK_SIZE];    /* Stack */
+  uint8_t SP;                       /* Stack Pointer */
+  uint8_t screen[C8_SCREEN_SIZE];   /* Display */
+  uint8_t keyboard[C8_KEYS];        /* Keyboard */
+  uint8_t* memory;                  /* RAM */
 } C8;
 
 
