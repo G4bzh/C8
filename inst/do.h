e79af146eac29f87e1ada5dec7135d7a58eaa3c6
G4bzh <jedemissionedusport@hotmail.fr>
2016-06-03 16:26:01 +0200

    do_0x5

--- inst/do.h
+++ inst/do.h
@@ -13,11 +13,9 @@
 
 int do_0x00E0(C8* c8, uint16_t nnn,  uint8_t n, uint8_t  x, uint8_t y, uint8_t kk);
 int do_0x00EE(C8* c8, uint16_t nnn,  uint8_t n, uint8_t  x, uint8_t y, uint8_t kk);
-
 int do_0x1nnn(C8* c8, uint16_t nnn,  uint8_t n, uint8_t  x, uint8_t y, uint8_t kk);
-
 int do_0x3xkk(C8* c8, uint16_t nnn,  uint8_t n, uint8_t  x, uint8_t y, uint8_t kk);
-
 int do_0x4xkk(C8* c8, uint16_t nnn,  uint8_t n, uint8_t  x, uint8_t y, uint8_t kk);
+int do_0x5xy0(C8* c8, uint16_t nnn,  uint8_t n, uint8_t  x, uint8_t y, uint8_t kk);
 
 #endif
